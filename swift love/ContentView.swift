9c38c0c58bd9731f6cb0b324fb36b4184466f9b5
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-01-26 21:18:55 +0100

    Initial Commit

---
+++ swift love/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  swift love
+//
+//  Created by  Ixart on 28/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+                
+            
+            Text("My first SwiftUl app")
+                .font(.largeTitle)
+            
+            Spacer()
+ 
+            
+
+            Text("sweet❤️")
+                .foregroundColor(.orange)
+                .font(.title)
+                            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift love/swift_loveApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_loveApp.swift
+//  swift love
+//
+//  Created by  Ixart on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_loveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
